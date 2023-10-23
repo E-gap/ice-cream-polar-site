5437bf64c774844156d9fc3d116a311d31db88fb
Evhen Gaponyuk <e-gap@ukr.net>
2023-10-23 16:11:08 +0300

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import logo from "../../images/logo.png";
+import facebook from "../../images/footer/facebook.svg";
+import twitter from "../../images/footer/twitter.svg";
+import instagram from "../../images/footer/instagram.svg";
+import linkedin from "../../images/footer/linkedin.svg";
 
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -33,8 +37,32 @@ const Footer = () => {
               Some food has looked so awful that it's looked like something that
               the dog's brought home.
             </p>
+            <p className={css.listLinksName}>Follow us</p>
+            <ul className={css.linkList}>
+              <li className={css.socialLinkItem}>
+                <a href="/">
+                  <img src={facebook} alt="facebook icon" />
+                </a>
+              </li>
+
+              <li className={css.socialLinkItem}>
+                <a href="/">
+                  <img src={twitter} alt="twitter icon" />
+                </a>
+              </li>
+              <li className={css.socialLinkItem}>
+                <a href="/">
+                  <img src={instagram} alt="instagram icon" />
+                </a>
+              </li>
+              <li className={css.socialLinkItem}>
+                <a href="/">
+                  <img src={linkedin} alt="linkedin icon" />
+                </a>
+              </li>
+            </ul>
           </div>
-          <div className={css.part2}>sdsd</div>
+          <div className={css.part2}>fdfdff</div>
           <div className={css.part3}>sdsd</div>
           <div className={css.part4}>sdsd</div>
         </div>
